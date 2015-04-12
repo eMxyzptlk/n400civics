0fc504c16d3136c55cbcc21a02e6928633db9603
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2015-04-11 21:26:10 -0700

    make it possible to select the questions from a list

--- main.go
+++ main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
 	"log"
-	"math/big"
+	"math/rand"
 	"os"
 	"os/exec"
 	"runtime"
-
-	crypto_rand "crypto/rand"
-	math_rand "math/rand"
+	"strconv"
+	"strings"
+	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 var (
-	minQuestion = flag.Int("min_question", 1, "The lowest question")
-	maxQuestion = flag.Int("max_question", 100, "The highest question")
-	cmd         *exec.Cmd
+	minQuestion  = flag.Int("min_question", 1, "The lowest question")
+	maxQuestion  = flag.Int("max_question", 100, "The highest question")
+	questionList = flag.String("question_list", "", "The list of the questions to display comma separated")
+	cmd          *exec.Cmd
 )
 
 func clearScreen() {
@@ -34,23 +38,31 @@ func clearScreen() {
 func main() {
 	flag.Parse()
 
-	randomBytes := make([]byte, 1024)
-	crypto_rand.Read(randomBytes)
-	seed, err := crypto_rand.Int(bytes.NewBuffer(randomBytes), big.NewInt(1E18))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	r := math_rand.New(math_rand.NewSource(seed.Int64()))
-	indexes := r.Perm(100)
+	indexes := rand.Perm(100)
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println("To jump between questions and answers, simply press Enter.")
 	clearScreen()
 	reader.ReadString('\n')
 
+	indexesSet := make(map[int]bool)
+	if *questionList != "" {
+		strList := strings.Split(*questionList, ",")
+		for _, s := range strList {
+			i, err := strconv.ParseInt(s, 10, 64)
+			if err != nil {
+				log.Fatalf("error decoding the question list: %s", err)
+			}
+			indexesSet[int(i)] = true
+		}
+	}
+
 	for _, index := range indexes {
 		if index == 0 || index < *minQuestion || index > *maxQuestion {
+			continue
+		}
+
+		if *questionList != "" && !indexesSet[index] {
 			continue
 		}
 
